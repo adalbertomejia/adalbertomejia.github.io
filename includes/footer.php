ea859004d69532b38e42b2ca0b4c47b44af4e192
JGLopezG <84732706+JGLopezG@users.noreply.github.com>
2023-06-05 23:54:04 -0600

    Descripción del cambio

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,17 @@
 <footer>
-    Todos los derechos reservados - UTVM &copy;
+    <div class="footer-content">
+        <div class="footer-left">
+            <h1>Universidad Tecnológica del Valle del Mezquital</h1>
+            <p>Todos los derechos reservados - UTVM &copy;</p>
+        </div>
+        <div class="footer-right">
+            <ul>
+                <li><a href="#">Acerca de</a></li>
+                <li><a href="#">Contacto</a></li>
+                <li><a href="#">Políticas de privacidad</a></li>
+            </ul>
+        </div>
+    </div>
 </footer>
 
 </body>
