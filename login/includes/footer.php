7ae318f24591f2a0d38574c8d6ea80780ee27ba1
Adalberto Mejía <adalbertomejiatrejo@gmail.com>
2023-06-14 09:00:27 -0600

    Separación de home-page y login

---
+++ login/includes/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="footer-content">
+        <div class="footer-left">
+            <h1>Universidad Tecnológica del Valle de México</h1>
+            <p>Todos los derechos reservados - UTVM &copy;</p>
+        </div>
+        <div class="footer-right">
+            <ul>
+                <li><a href="#">Acerca de</a></li>
+                <li><a href="#">Contacto</a></li>
+                <li><a href="#">Políticas de privacidad</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
